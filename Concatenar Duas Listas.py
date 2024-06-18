4bad0cd52804180b6d2609e44a1d1d4c6c5b5305
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-06-18 10:16:26 -0300

    Add files via upload

---
+++ Concatenar Duas Listas.py
@@ -0,0 +1,8 @@
+lista1 = input("Digite os elementos da primeira lista separados por espaço: ").split()
+lista2 = input("Digite os elementos da segunda lista separados por espaço: ").split()
+
+
+listaconcatenada = lista1 + lista2
+
+
+print("Listas concatenadas:", listaconcatenada)
