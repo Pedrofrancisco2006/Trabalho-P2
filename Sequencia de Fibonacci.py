33c9c5c883f894da9759b1d83c05051694db7cb9
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-06-20 08:09:08 -0300

    exercicio 3.py

--- Sequencia de Fibonacci.py
+++ Sequencia de Fibonacci.py
@@ -1,32 +1,15 @@
-def fibonacci_ate(numero):
-    fib = [0, 1]  
-    
-   
-    while fib[-1] <= numero:
-        fib.append(fib[-1] + fib[-2])
-    
-    
-    if fib[-1] > numero:
-        fib.pop()
+sequencia = int(input('Numero que seja maior que 0 que deseja ver na sequencia: '))
 
-    return fib
+ultimo = 1
+penultimo = 0
 
+lista = [0,1]
 
-while True:
-    try:
-        num = int(input("Digite um número inteiro positivo para exibir a sequência de Fibonacci (ou '0' para sair): "))
-        if num == 0:
-            print("Programa encerrado.")
-            break
-        if num < 0:
-            print("Por favor, digite um número inteiro positivo.")
-            continue
-        
-       
-        fib_sequence = fibonacci_ate(num)
-        
-       
-        print(f"A sequência de Fibonacci até {num} é: {fib_sequence}")
-    
-    except ValueError:
-        print("Entrada inválida. Por favor, digite um número inteiro.")
+for contador in range(2 , sequencia):
+    termo = ultimo + penultimo
+    lista.append (termo)
+    penultimo = ultimo
+    ultimo = termo
+    contador = contador + 1
+
+print(lista)
